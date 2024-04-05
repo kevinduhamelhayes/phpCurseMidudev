c77b356c897ed55b68c39401d3e2085d62f6bd6a
kevinduhamelhayes <kevinduhamelh@gmail.com>
2024-04-04 21:17:45 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$name = "John";
+echo "Hello $name";
+$age = 25;
+echo "You are $age years old";
+//concatenador
+echo "Hello " . $name . "You are " . $age . " years old";
+//sumador
+echo "Hello " . $name . "You are " . ($age + 1) . " years old";
+
+var_dump($name);
+var_dump($age);
+//var_dump($name, $age);
+?>
